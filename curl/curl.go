0b680a080267b39f09e2c5d2947673892564a8bc
张海生 <zhanghs@wifi.com>
2019-07-29 13:34:15 +0800

    init

---
+++ curl/curl.go
@@ -0,0 +1,48 @@
+package curl
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+
+
+
+func Get(url string , parms url.Values) string{
+	resp, err := http.Get(url+ "?"+parms.Encode())
+	if err != nil {
+		// handle error
+		return ""
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// handle error
+		return ""
+	}
+
+	return string(body)
+	//fmt.Println(string(body))
+	//fmt.Printf("%s",body)
+}
+
+func Post(url string,parms url.Values) string{
+	resp, err := http.PostForm(url,parms)
+
+	if err != nil {
+		// handle error
+		return ""
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// handle error
+		return ""
+	}
+	return string(body)
+	//fmt.Println(string(body))
+	//fmt.Printf("%s",body)
+}
