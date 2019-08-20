bf8982af1478b39eec800cfdd6c14e42bcc05a93
张海生 <zhanghs@wifi.com>
2019-08-20 17:39:22 +0800

    zuixin

--- main/main.go
+++ main/main.go
@@ -9,6 +9,17 @@ import (
 )
 
 var (
+//CRON Expression Format
+//A cron expression represents a set of times, using 6 space-separated fields.
+//
+//Field name   | Mandatory? | Allowed values  | Allowed special characters
+//----------   | ---------- | --------------  | --------------------------
+//Seconds      | Yes        | 0-59            | * / , -
+//Minutes      | Yes        | 0-59            | * / , -
+//Hours        | Yes        | 0-23            | * / , -
+//Day of month | Yes        | 1-31            | * / , - ?
+//Month        | Yes        | 1-12 or JAN-DEC | * / , -
+//Day of week  | Yes        | 0-6 or SUN-SAT  | * / , - ?
 	CRON  *cron.Cron
 	everyMinuteFifteenSecond = "15 * * * * *"
 	everyFourSecond = "*/4 * * * * *"
