b4f2009385792be251df3c4d27edff5063ae5b7f
张海生 <zhanghs@wifi.com>
2019-09-10 12:05:43 +0800

    股票 拼音和简称

--- xueqiu/symbol.go
+++ xueqiu/symbol.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/hisheng/chang/db"
 	"github.com/jinzhu/gorm"
+	"github.com/mozillazg/go-pinyin"
 	"net/url"
 	"strconv"
 )
@@ -21,6 +22,9 @@ type Symbol_ struct {
 	Area_name string //地区
 	Tick_size float64
 	Indcode string
+	Stock_number string `sql:"type:varchar(10)"`
+	Stock_abbr string  `sql:"type:varchar(20)"`
+	Stock_pinyin string  `sql:"type:varchar(100)"`
 }
 
 func (s Symbol_) getTableName() string {
@@ -45,10 +49,21 @@ func (s Symbol_) Gets() (ss []Symbol_){
 
 //因为name 和 area_name 和注册地 有可能会变化，所以有的时候 是修改
 func (s Symbol_)Add() Symbol_{
-	//处理名字前缀XD
+	//1处理名字前缀XD
 	if s.Name[:2] == "XD"{
 		s.Name = s.Name[2:]
 	}
+	//2处理stock number 只针对 SH 和 SZ 交易所
+	s.Stock_number = s.Symbol[2:]
+	//2 处理拼音相关
+	a := pinyin.NewArgs()
+	pys := pinyin.Pinyin(s.Name, a)
+
+	for _,py := range pys{
+		s.Stock_pinyin = s.Stock_pinyin + py[0]
+		s.Stock_abbr = s.Stock_abbr + py[0][:1]
+	}
+
 	is := s.FindOne(s.Symbol)
 	if is.ID == 0 {
 		db.DB.Table(s.getTableName()).Create(&s)
