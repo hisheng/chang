2ca18635f30e6f64cff2c92fe4d69735137efdac
张海生 <zhanghaisheng@qimao.com>
2021-05-28 16:39:50 +0800

    自动创建数据库

--- db/db.go
+++ db/db.go
@@ -1,6 +1,8 @@
 package db
 
 import (
+	"database/sql"
+	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/hisheng/chang/conf"
 	"github.com/jinzhu/gorm"
@@ -8,37 +10,58 @@ import (
 	"time"
 )
 
-
 // gorm.Model definition
 type Model struct {
-	Id int
+	Id        int
 	CreatedAt time.Time `gorm:"column:created_at;"`
-	UpdatedAt time.Time  `gorm:"column:updated_at;"`
-	DeletedAt time.Time  `gorm:"column:deleted_at;"`
-
+	UpdatedAt time.Time `gorm:"column:updated_at;"`
+	DeletedAt time.Time `gorm:"column:deleted_at;"`
 }
 
 var (
-	DB *gorm.DB
-	err error
+	DB       *gorm.DB
+	err      error
 	ReportDb *gorm.DB
 )
 
 func GetDb() {
+	initCreateDb()
 	initChangDb()
 	initReportDb()
 	//initTestDb()
 	//return db
 }
 
-func initChangDb()  {
+func initCreateDb() {
+	db, err := sql.Open("mysql", conf.Conf.MysqlConf.User+
+		":"+conf.Conf.MysqlConf.Password+
+		"@tcp("+conf.Conf.MysqlConf.Ip+
+		conf.Conf.MysqlConf.Port+")/"+
+		"?charset=utf8mb4&parseTime=true&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	_, err = db.Exec("create database IF NOT EXISTS chang character set utf8mb4 collate utf8mb4_general_ci")
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = db.Exec("create database IF NOT EXISTS chang_report character set utf8mb4 collate utf8mb4_general_ci")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func initChangDb() {
+	fmt.Println(conf.Conf)
 	DB, err = gorm.Open(
 		"mysql",
-		conf.Conf.MysqlConf.User +
-			":"+conf.Conf.MysqlConf.Password +
-			"@tcp("+conf.Conf.MysqlConf.Ip +
-			conf.Conf.MysqlConf.Port+")/" +
-			conf.Conf.MysqlConf.Database +
+		conf.Conf.MysqlConf.User+
+			":"+conf.Conf.MysqlConf.Password+
+			"@tcp("+conf.Conf.MysqlConf.Ip+
+			conf.Conf.MysqlConf.Port+")/"+
+			conf.Conf.MysqlConf.Database+
 			"?charset=utf8mb4&parseTime=true&loc=Local")
 	if err != nil {
 		log.Fatal(err)
@@ -46,22 +69,17 @@ func initChangDb()  {
 	}
 }
 
-
-
-func initReportDb()  {
+func initReportDb() {
 	ReportDb, err = gorm.Open(
 		"mysql",
-		conf.Report.MysqlReport.User +
-			":"+conf.Report.MysqlReport.Password +
-			"@tcp("+conf.Report.MysqlReport.Ip +
-			conf.Report.MysqlReport.Port+")/" +
-			conf.Report.MysqlReport.Database +
+		conf.Report.MysqlReport.User+
+			":"+conf.Report.MysqlReport.Password+
+			"@tcp("+conf.Report.MysqlReport.Ip+
+			conf.Report.MysqlReport.Port+")/"+
+			conf.Report.MysqlReport.Database+
 			"?charset=utf8mb4&parseTime=true&loc=Local")
 	if err != nil {
 		log.Fatal(err)
 		defer ReportDb.Close()
 	}
 }
-
-
-
--- xueqiu/request.go
+++ xueqiu/request.go
@@ -8,32 +8,31 @@ import (
 	"net/url"
 )
 
-var XueqiuRequest  Request_
+var XueqiuRequest Request_
 
 type Request_ struct {
-	SearchUrl string
+	SearchUrl   string
 	SearchParms url.Values
 }
 
-func Get(url string,parms url.Values) string {
+func Get(url string, parms url.Values) string {
 	cookies := conf.XueqiuCookie.Cookies
 
 	client := &http.Client{}
 
-	req, err := http.NewRequest("GET", url+ "?"+parms.Encode(), nil)
-	if err!=nil	{
+	req, err := http.NewRequest("GET", url+"?"+parms.Encode(), nil)
+	if err != nil {
 		fmt.Println("获取地址错误")
 	}
 	req.Header.Set("Cookie", cookies)
 	//req.Header.Add("Agent",GetRandomUserAgent() )
 	resp, err := client.Do(req)
-	if err!=nil {
+	if err != nil {
 		fmt.Println("登录错误")
 	}
 	resp_byte, err := ioutil.ReadAll(resp.Body)
 
 	defer resp.Body.Close()
-
 
 	return string(resp_byte)
 
@@ -49,11 +48,10 @@ func GetSymbol(symbol string) string {
 
 }
 
-func createTable()  {
+func createTable() {
 	Lirunbiao.CreateTable()
 	Zichanfuzhai.CreateTable()
 	Xianjinliu.CreateTable()
 	StockChartModel.CreateTable()
 	StockQuote.CreateTable()
 }
-
